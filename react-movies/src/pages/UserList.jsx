dd7ea12469d6eda118d29b7dcf9c4a1044ad7c7a
Solaa08 <solarium_08@hotmail.fr>
2022-09-17 23:36:31 +0200

    first commit

---
+++ react-movies/src/pages/UserList.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
+import Card from "../components/Card";
+import Header from "../components/Header";
+
+const UserList = () => {
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d&language=fr-FR`
+        )
+        .then((res) => setListData((listData) => [...listData, res.data]));
+    }
+  }, []);
+
+  useEffect(() => {
+      console.log(listData)
+  }, [listData])
+
+  const deleteStorage = (movie) => {
+    let storedData = window.localStorage.movies.split(",");
+
+    let newData = storedData.filter((id) => id != movie.id);
+
+    window.localStorage.movies = newData;
+    setListData(listData.filter(data => data.id != movie.id));
+  };
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>
+        Coups de coeur <span>💖</span>
+      </h2>
+      <div className="result">
+        {listData.length > 0 ? (
+          listData.map((movie) => <Card movie={movie} key={movie.id} deleteStorage={deleteStorage} />)
+        ) : (
+          <h2>Aucun coup de coeur pour le moment</h2>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
